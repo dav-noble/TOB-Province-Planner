0a047e86e878eda2d573891961848b0aa5c4de74
dav-noble <davidnoble26@gmail.com>
2025-08-11 15:47:22 +0100

    Update: clear errors in views.py

--- planner/views.py
+++ planner/views.py
@@ -17,10 +17,6 @@ from .forms import (
 )
 
 
-
-# Create your views here.
-
-
 class PlanList(generic.ListView):
     template_name = "planner/index.html"
     paginate_by = 6
@@ -50,7 +46,10 @@ def plan_detail(request, slug):
     plan = get_object_or_404(queryset, slug=slug)
 
     if request.user != plan.author:
-        messages.error(request, "You do not have permission to view this plan.")
+        messages.error(
+            request,
+            "You do not have permission to view this plan."
+        )
         return redirect("home")
 
     return render(
@@ -65,7 +64,10 @@ def plan_form(request, slug=None):
         plan = get_object_or_404(Plan, slug=slug)
         # Check if the user is the author
         if request.user != plan.author:
-            messages.error(request, "You do not have permission to edit this plan.")
+            messages.error(
+                request,
+                "You do not have permission to edit this plan."
+            )
             return redirect("home")
         primary_instance = plan.primary_building
         secondary_instance_1 = plan.secondary_building_1
@@ -90,15 +92,50 @@ def plan_form(request, slug=None):
 
     if request.method == "POST":
         plan_form = PlanForm(data=request.POST, instance=plan)
-        primary_form = PrimaryForm(data=request.POST, instance=primary_instance)
-        secondary_form_1 = SecondaryForm1(data=request.POST, prefix="sec1", instance=secondary_instance_1)
-        secondary_form_2 = SecondaryForm2(data=request.POST, prefix="sec2", instance=secondary_instance_2)
-        secondary_form_3 = SecondaryForm3(data=request.POST, prefix="sec3", instance=secondary_instance_3)
-        tertiary_form_1 = TertiaryForm1(data=request.POST, prefix="ter1", instance=tertiary_instance_1)
-        tertiary_form_2 = TertiaryForm2(data=request.POST, prefix="ter2", instance=tertiary_instance_2)
-        tertiary_form_3 = TertiaryForm3(data=request.POST, prefix="ter3", instance=tertiary_instance_3)
-        tertiary_form_4 = TertiaryForm4(data=request.POST, prefix="ter4", instance=tertiary_instance_4)
-        tertiary_form_5 = TertiaryForm5(data=request.POST, prefix="ter5", instance=tertiary_instance_5)
+        primary_form = PrimaryForm(
+            data=request.POST,
+            instance=primary_instance
+        )
+        secondary_form_1 = SecondaryForm1(
+            data=request.POST,
+            prefix="sec1",
+            instance=secondary_instance_1
+        )
+        secondary_form_2 = SecondaryForm2(
+            data=request.POST,
+            prefix="sec2",
+            instance=secondary_instance_2
+        )
+        secondary_form_3 = SecondaryForm3(
+            data=request.POST,
+            prefix="sec3",
+            instance=secondary_instance_3
+        )
+        tertiary_form_1 = TertiaryForm1(
+            data=request.POST,
+            prefix="ter1",
+            instance=tertiary_instance_1
+        )
+        tertiary_form_2 = TertiaryForm2(
+            data=request.POST,
+            prefix="ter2",
+            instance=tertiary_instance_2
+        )
+        tertiary_form_3 = TertiaryForm3(
+            data=request.POST,
+            prefix="ter3",
+            instance=tertiary_instance_3
+        )
+        tertiary_form_4 = TertiaryForm4(
+            data=request.POST,
+            prefix="ter4",
+            instance=tertiary_instance_4
+        )
+        tertiary_form_5 = TertiaryForm5(
+            data=request.POST,
+            prefix="ter5",
+            instance=tertiary_instance_5
+        )
 
         if plan_form.is_valid():
             plan = plan_form.save(commit=False)
@@ -110,31 +147,22 @@ def plan_form(request, slug=None):
 
             if primary_form.is_valid():
                 plan.primary_building = primary_form.save()
-            
             if secondary_form_1.is_valid():
                 plan.secondary_building_1 = secondary_form_1.save()
-            
             if secondary_form_2.is_valid():
                 plan.secondary_building_2 = secondary_form_2.save()
-            
             if secondary_form_3.is_valid():
                 plan.secondary_building_3 = secondary_form_3.save()
-
             if tertiary_form_1.is_valid():
                 plan.tertiary_building_1 = tertiary_form_1.save()
-
             if tertiary_form_2.is_valid():
                 plan.tertiary_building_2 = tertiary_form_2.save()
-
             if tertiary_form_3.is_valid():
                 plan.tertiary_building_3 = tertiary_form_3.save()
-
             if tertiary_form_4.is_valid():
                 plan.tertiary_building_4 = tertiary_form_4.save()
-
             if tertiary_form_5.is_valid():
                 plan.tertiary_building_5 = tertiary_form_5.save()
-            
             is_edit = plan.pk is not None  # True if editing, False if creating
 
             plan.save()
@@ -159,14 +187,30 @@ def plan_form(request, slug=None):
     else:
         plan_form = PlanForm(instance=plan)
         primary_form = PrimaryForm(instance=primary_instance)
-        secondary_form_1 = SecondaryForm1(prefix="sec1", instance=secondary_instance_1)
-        secondary_form_2 = SecondaryForm2(prefix="sec2", instance=secondary_instance_2)
-        secondary_form_3 = SecondaryForm3(prefix="sec3", instance=secondary_instance_3)
-        tertiary_form_1 = TertiaryForm1(prefix="ter1", instance=tertiary_instance_1)
-        tertiary_form_2 = TertiaryForm2(prefix="ter2", instance=tertiary_instance_2)
-        tertiary_form_3 = TertiaryForm3(prefix="ter3", instance=tertiary_instance_3)
-        tertiary_form_4 = TertiaryForm4(prefix="ter4", instance=tertiary_instance_4)
-        tertiary_form_5 = TertiaryForm5(prefix="ter5", instance=tertiary_instance_5)
+        secondary_form_1 = SecondaryForm1(
+            prefix="sec1", instance=secondary_instance_1
+            )
+        secondary_form_2 = SecondaryForm2(
+            prefix="sec2", instance=secondary_instance_2
+            )
+        secondary_form_3 = SecondaryForm3(
+            prefix="sec3", instance=secondary_instance_3
+            )
+        tertiary_form_1 = TertiaryForm1(
+            prefix="ter1", instance=tertiary_instance_1
+            )
+        tertiary_form_2 = TertiaryForm2(
+            prefix="ter2", instance=tertiary_instance_2
+            )
+        tertiary_form_3 = TertiaryForm3(
+            prefix="ter3", instance=tertiary_instance_3
+            )
+        tertiary_form_4 = TertiaryForm4(
+            prefix="ter4", instance=tertiary_instance_4
+            )
+        tertiary_form_5 = TertiaryForm5(
+            prefix="ter5", instance=tertiary_instance_5
+            )
 
     return render(
         request,
